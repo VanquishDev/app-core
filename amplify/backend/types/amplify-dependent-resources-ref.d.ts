6a616b60c60e707c0df93d019c4cf21cba492658
Renato T. Carvalho Jr <renato@vanquish.com.br>
2023-03-02 16:20:25 -0300

    add api

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "AppCoreApi": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
